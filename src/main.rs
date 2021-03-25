51915fb1555806eeb60d72b1a494cc07a25a4bda
Matthew Torrence <matt@torrencefamily.net>
2021-03-25 18:01:36 -0400

    for kyle

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
 
 use addcomb_comp::setlike::SetLike;
 use addcomb_comp::setlike::Group;
+use addcomb_comp::exactset;
+use addcomb_comp::comb::gcd;
 
 /// Compute whether there is a set A of size two such that [ia, ib]A = G
 /// If there is such a set, this function returns one of these sets. Otherwise, it returns None.
-fn do_two_elements_span<S: SetLike>(n: S::Group, (ia, ib): (u32, u32)) -> Option<S> {
-    let g_size = n.gsize();
-    for a in S::each_set_exact(n.clone(), 2) {
-        let size = a.hfold_interval_signed_sumset((ia, ib), n.clone()).size();
+fn do_two_elements_span(k: u32, s: u32) -> Option<Vec<GElem>> {
+    let g = Rc::new(vec![2, 2 * k]);
+    let g_size = g.gsize();
+
+    if 4*k > 2*s*s + 2*s + 1 {
+        return None;
+    }
+
+    for a in <Vec<GElem> as SetLike>::each_set_exact(g.clone(), 2) {
+        let p = &a[0];
+        let q = &a[1];
+        let c = p.0[0];
+        let x = p.0[1];
+        let d = q.0[0];
+        let y = q.0[1];
+
+        // Optimization 1
+        if c == 0 && d == 0 {
+            continue;
+        }
+
+        // Optimization 2
+        if gcd(2*k, gcd(x, y)) != 1 {
+            continue;
+        }
+
+        // Optimization 3
+        // Compute if x and y are not in [k / s, -k / s]
+        let lb = k / s;
+        let ub = 2*k - (k / s);
+        if (x < lb || x > ub) && (y < lb || y > ub) {
+            continue;
+        }
+
+        let size = exactset::hfold_interval_signed_sumset(&a, (0, s), g.clone()).len() as u32;
         if size == g_size {
             return Some(a);
         }
@@ -19,21 +52,22 @@ use std::rc::Rc;
 use addcomb_comp::exactset::GElem;
 
 fn main() {
-    let k = 5;
-    // Our group
-    let g = Rc::new(vec![2, 2 * k]);
+    for s in 2..100 {
+        println!("s = {}", s);
+        for k in 5..((2*s*s + 2*s + 1) / 4) {
+            // Compute do_two_elements_span
+            let comp_result =
+                do_two_elements_span(k, s);
 
-    // Compute do_two_elements_span
-    // Note I'm assuming you're taking the [0, 3] fold signed sumset!
-    let comp_result =
-        do_two_elements_span::<Vec<GElem>>(g, (0, 3));
-
-    match comp_result {
-        Some(set) => {
-            println!("Found a set that spans our group: {:?}", set);
-        },
-        None => {
-            println!("No sets of size two span the group!");
-        },
+            match comp_result {
+                Some(set) => {
+                    println!("{}: {:?}", k, set);
+                },
+                None => {
+                    // println!("~{}", k);
+                },
+            }
+        }
     }
 }
+
